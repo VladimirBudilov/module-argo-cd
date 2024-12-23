eec5f7bbd634c8a94135c9cfb6a85d6067a7311a
Vladimir Budilov <budilo.w.ladimir@gmail.com>
2024-12-23 21:46:37 +0400

    WIP

--- main.tf
+++ main.tf
@@ -31,9 +31,21 @@ resource "helm_release" "argocd" {
   chart      = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
   namespace  = kubernetes_namespace.argo_ns.metadata[0].name
+  version    = "5.34.2" # Укажите нужную версию
 
   set {
     name  = "controller.extraArgs"
     value = "insecure"
+  }
+
+  set {
+    name  = "crds.install"
+    value = true
+  }
+
+  # Добавлено автоматическое управление RBAC, если нужно
+  set {
+    name  = "rbac.create"
+    value = true
   }
 }
