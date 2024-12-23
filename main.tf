d98a51c4d4857442da4129ce88cfccc4c93ae514
Vladimir Budilov <budilo.w.ladimir@gmail.com>
2024-12-23 21:07:59 +0400

    feat: config argo cd

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "kubernetes" {
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+  host                   = var.kubernetes_cluster_endpoint
+  exec {
+    api_version = "client.authentication.k8s.io/v1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    cluster_ca_certificate = base64decode(var.kubernetes_cluster_cert_data)
+    host                   = var.kubernetes_cluster_endpoint
+    exec {
+      api_version = "client.authentication.k8s.io/v1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", var.kubernetes_cluster_name]
+    }
+  }
+}
+
+resource "kubernetes_namespace" "argo_ns" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "msur"
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  namespace  = kubernetes_namespace.argo_ns.metadata[0].name
+
+  set {
+    name  = "controller.extraArgs"
+    value = "insecure"
+  }
+}
